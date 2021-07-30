adc7498d7c231dce33d5d0632720a813d23ae25f
daimessdn@gmail.com <daimessdn@gmail.com>
2021-07-30 20:28:07 +0700

    added endpoint for add WTI weekly prices from oilprices.com

--- index.js
+++ index.js
@@ -31,10 +31,63 @@ app.get("/csrf", async (request, response) => {
   }
 });
 
-app.get("/wti", (request, response) => {
-  console.log(request.body);
+app.get("/:blend/:period", async (request, response) => {
+  let blend_code, period;
 
-  response.end;
+  // switch blend code based on blend param
+  switch (request.params.blend) {
+    case "wti":
+      blend_code = 45;
+      break;
+    default:
+      break;
+  }
+
+  // switch period based on period param
+  switch (request.params.period) {
+    case "weekly":
+      period = 3;
+      break;
+    default:
+      break;
+  }
+
+  try {
+    // get CSRF token from /csrf route
+    const csrf_token = await axios
+      .get("http://192.168.1.9:3000/csrf")
+      .then((result) => {
+        return result.data.datacsrf_token;
+      });
+
+    const blend_data = await axios.post(
+      `https://oilprice.com/freewidgets/json_get_oilprices`,
+      `blend_id=${blend_code}&period=${period}&op_csrf_token=${csrf_token}`,
+      {
+        headers: {
+          Accept: "application/json, text/javascript, */*; q=0.01",
+          "Accept-Language": "en-US,en;q=0.5",
+          "Accept-Encoding": "gzip, deflate, br",
+          "Content-Type": "application/x-www-form-urlencoded",
+          "Sec-Fetch-Dest": "empty",
+          "Sec-Fetch-Mode": "cors",
+          "Sec-Fetch-Site": "same-origin",
+          "Sec-GPC": 1,
+          TE: "trailers",
+          "X-Requested-With": "XMLHttpRequest",
+        },
+      }
+    );
+
+    response.status(200).json(blend_data.data);
+    console.log(blend_data.data);
+  } catch (error) {
+    console.log(error);
+
+    response.json({
+      message: "request failed",
+    });
+  }
 });
 
 app.listen(3000, () => {
